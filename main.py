07455e7358e699da07c660ee5cd6a2a1967e9c20
eiloncohen <eiloncohen20@gmail.com>
2022-01-11 18:26:30 +0200

    add gui for statistics

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ import plotly.express as px
 table_obj = pandas.read_csv("stocks_table.csv")
 stocks_list = table_obj["stock name"].tolist()
 
-
 # connect to interactive brokers
 ib = IB()
 ib.connect('127.0.0.1', 7497, clientId=1)
 
 
-def update_csv_table(stocks_table, j, stock_name, enter_price, is_down_5_percents, is_down_10_percents, is_down_15_percents, is_down_20_percents, is_order_succeed):
+def update_csv_table(stocks_table, j, stock_name, enter_price, is_down_5_percents, is_down_10_percents,
+                     is_down_15_percents, is_down_20_percents, is_order_succeed):
     stocks_table.loc[j, 'stock name'] = stock_name
     stocks_table.loc[j, 'enter price'] = enter_price
     stocks_table.loc[j, 'down 5% percents'] = is_down_5_percents
@@ -69,53 +69,57 @@ def get_real_time_info_on_stock(ticker, i):
             is_order_succeed = "yes"
 
     print(market_data.last)
-    print([stock_name,enter_price,is_down_5_percents,is_down_10_percents,is_down_15_percents, is_down_20_percents,is_order_succeed])
-    update_csv_table(table_obj, i, stock_name, enter_price, is_down_5_percents, is_down_10_percents, is_down_15_percents, is_down_20_percents, is_order_succeed)
-
-# for i, stock in enumerate(stocks_list):
-#     get_real_time_info_on_stock(stock, i)
-
-
-#create the result of table
-# table_obj.to_csv("stocks_table.csv")
-
-stocks_table_csv = pandas.read_csv("stocks_table.csv")
-st.set_page_config(page_title = "Shagim followers stocks")
-print(stocks_table_csv)
+    print([stock_name, enter_price, is_down_5_percents, is_down_10_percents, is_down_15_percents, is_down_20_percents,
+           is_order_succeed])
+    update_csv_table(table_obj, i, stock_name, enter_price, is_down_5_percents, is_down_10_percents,
+                     is_down_15_percents, is_down_20_percents, is_order_succeed)
 
 
+for i, stock in enumerate(stocks_list):
+    get_real_time_info_on_stock(stock, i)
+
+    # create the result of table
+table_obj.to_csv("stocks_table.csv")
+
+stocks_table_csv = pandas.read_csv("stocks_table.csv", index=False)
+
+# set config to App
+st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
 
 
+# ---- READ CSV ----
+@st.cache
+def get_data_from_excel():
+    df = pd.read_csv("stocks_table.csv")
+    return df
 
 
+df = get_data_from_excel()
+stocks_name = df["stock name"].to_list()
+print(stocks_name)
+# ---- SIDEBAR ----
+st.sidebar.header("")
 
+choosen_stock = st.selectbox(
+        'Select the Stock:',
+        options=df["stock name"].unique(),
+)
 
+st.write('You selected:', choosen_stock)
+specific_row = df[df["stock name"] == choosen_stock]
+count_of_success_orders = df[df["order succeed?"] == "yes"].count()
+count_of_failed_orders = df[df["order succeed?"] == "no"].count()
 
+# ---- MAINPAGE ----
+st.title(":bar_chart: Sales Dashboard")
+st.write(specific_row)
+st.write(int(count_of_success_orders["order succeed?"]))
+data = {
+    'title' : ['success orders', 'failed orders'],
+    'amount': [int(count_of_success_orders["order succeed?"]), int(count_of_failed_orders["order succeed?"])]
+}
 
-
-
-#
-# stock = Stock('DKNG', 'SMART', 'USD')
-#
-#
-# market_data = ib.reqMktData(stock, "", False, False)
-
-
-# ib.sleep(2)
-# print(market_data)
-# ib.pendingTickersEvent += get_real_time_info_on_stock
-#
-# ib.run()
-
-
-
-
-
-#get history info on stock
-
-# convert to pandas dataframe:
-# df = util.df(bars)
-# print(df)
-# bars = ib.reqHistoricalData(
-#     stock, endDateTime='', durationStr='30 D',
-#     barSizeSetting='1 hour', whatToShow='MIDPOINT', useRTH=True)
+orders_table = pd.DataFrame.from_dict(data)
+print(orders_table)
+fig = px.pie(orders_table, values='amount', names='title', title='All finished orders')
+st.plotly_chart(fig)
